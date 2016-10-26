79058227e21ce0f9ea14cd700e3af126eee557e5
Fabricio Flores <fabrifloresg@gmail.com>
2016-10-26 18:28:32 +0200

    Sort results

--- app/src/main/java/es/upm/miw/SolitarioCelta/ShowResults.java
+++ app/src/main/java/es/upm/miw/SolitarioCelta/ShowResults.java
@@ -9,8 +9,12 @@ import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import es.upm.miw.SolitarioCelta.R;
+import es.upm.miw.SolitarioCelta.models.Result;
 
 public class ShowResults extends Activity {
 
@@ -18,15 +22,24 @@ public class ShowResults extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.scores);
 		TextView textView = (TextView) findViewById(R.id.scores);
-
+		ArrayList<Result> results = new ArrayList<Result>();
 		String finalText = "";
 		try{
 			BufferedReader fin = new BufferedReader(
 					new InputStreamReader(openFileInput(getString(R.string.scores))));
 			String linea = fin.readLine();
 			while (linea != null) {
-				finalText += linea + "\n";
+				results.add(new Result(linea));
 				linea = fin.readLine();
+			}
+			Collections.sort(results, new Comparator<Result>() {
+				@Override
+				public int compare(Result lhs, Result rhs) {
+					return lhs.getScore() < rhs.getScore() ? -1 : (lhs.getScore() > rhs.getScore() ) ? 1 : 0;
+				}
+			});
+			for(int i=0; i < results.size(); i++){
+				finalText += results.get(i).toString() + "\n";
 			}
 			fin.close();
 		}catch(IOException e){
